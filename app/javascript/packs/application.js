23a7f8d50a70a6620de33486d3dab881d261d278
Christian Zamora <christianzamoraburgos@gmail.com>
2020-12-28 15:40:24 -0600

    added Style sheets architecture

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,4 +16,4 @@ import 'bootstrap';
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log("hello world");
+console.log("hello world again");
