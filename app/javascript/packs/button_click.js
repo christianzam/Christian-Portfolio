44480051807fda2cecee8105bcfdc2c9c0363585
Christian Zamora <christianzamoraburgos@gmail.com>
2021-01-07 01:01:24 -0600

    fixed buttonClick function for banner text

--- app/javascript/packs/button_click.js
+++ app/javascript/packs/button_click.js
@@ -1,33 +1,17 @@
 const buttonClick = () => {
   const button = document.querySelector("#clicker");
   const ball = document.querySelector("#target");
+   
     button.addEventListener("click", () => {
-      console.log("you have pressed")
-      button.classList.remove("animate")
-      
-      ball.classList.add("animate")
-      //ball.setTimeout(100)
-      
+      console.log("times pressed")
+      window.requestAnimationFrame(function() {
+        document.getElementById("target").classList.remove('animate');   
+        window.requestAnimationFrame(function() {
+          document.getElementById("target").classList.add('animate');
+        });
+      });
     });
 };
 
 export { buttonClick };
 
-/*
-const buttonClick = () => {
-  const xxx = document.querySelector("#clicker");
-    xxx.addEventListener("click", () => {
-      console.log("BUTTON pressed")
-      xxx.removeClass("animate")
-      setTimeout("#target.addClass('animate');",100)
-    });
-};
-
-
-
-$('#clicker').click(function(){
-  $target = $('#target');
-  $target.removeClass('animate');
-  setTimeout("$target.addClass('animate');",100)
-});
-*/
