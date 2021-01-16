1a514f671e953702329f6de6df8f4e37483ee74d
Christian Zamora <christianzamoraburgos@gmail.com>
2021-01-15 18:46:41 -0600

    re ordered layout ot bio to make it responsive

--- app/javascript/components/dynamic_text_typed_js.js
+++ app/javascript/components/dynamic_text_typed_js.js
@@ -2,9 +2,9 @@ import Typed from 'typed.js';
 
 const dynamicTextTypedJS = () => {
   new Typed('#nav-typed-text', {
-    strings: ["<span style='color:white'> Hello world </span>","<span style='color:white'> I <span style='color:yellow'> am </span> </span><span style= 'color:white'> ^100 &nbsp; &nbsp; Christian<span style='color:white'>&nbsp;Z<span style='color:yellow'>am<span style='color:white'>ora</span></span></span></span>"],
+    strings: ["<span style='color:white'> Hello world </span>","<span style='color:white'> I <span style='color:yellow'> am </span> </span><span style= 'color:white'> &nbsp; &nbsp; Christian<span style='color:white'>Z<span style='color:yellow'>am<span style='color:white'>ora</span></span></span></span>"],
     typeSpeed: 100,
-    startDelay: 200,
+    startDelay: 170,
     loop: false,
     
   }); 
