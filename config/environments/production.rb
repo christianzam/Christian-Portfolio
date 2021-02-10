91046179aa3090c1633528ed7110dc5503b9441e
Christian Zamora <christianzamoraburgos@gmail.com>
2021-02-10 14:35:56 -0600

    changed catche in production to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   config.action_mailer.default charset: 'utf-8'
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com',
+    domain: ENV['GMAIL_DOMAIN'],
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_USERNAME'],
