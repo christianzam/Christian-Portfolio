050be5291571b258c7c64402c9ef03eeb5cc9e62
Christian Zamora <christianzamoraburgos@gmail.com>
2021-02-11 13:28:38 -0600

    added SE variables to development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,11 +40,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: ENV['GMAIL_DOMAIN'],
+    domain: ENV['SE_HEROKU_DOMAIN'],
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD']
+    password: ENV['SE_GMAIL_PASSWORD']
   }
 
   # Print deprecation notices to the Rails logger.
