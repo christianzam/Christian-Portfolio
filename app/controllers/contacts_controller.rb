080a39d6884182f78c571be3e3cff1d2d7de6b88
Christian Zamora <christianzamoraburgos@gmail.com>
2021-02-11 23:29:49 -0600

    added meta content and favicon

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       # flash.now[:notice] = 'Thank you for your message. I will contact pretty you soon!'
-      redirect_to new_contact_path, alert: 'BRILLIANT!, I will contact you pretty soon, thank you.'
+      redirect_to new_contact_path, alert: "BRILLIANT!, " + "" + " I will contact you pretty soon, thank you."
     else
       # flash.now[:error] = 'Oops! something is wrong, can you please send me an email instead?'
       # render :new
